c9381459b995985921b0ea65b99ffbada3a13732
Vuk Dzopalic <47774567+vukdz@users.noreply.github.com>
2019-02-19 18:56:30 +0100

    Initial commit

---
+++ SlotMachine/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace SlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.scoreTB = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.defScoreTB = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(120, 123);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(330, 123);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(542, 123);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(303, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "spin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(606, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.scoreTB);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.defScoreTB);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 384);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(802, 100);
+            this.panel1.TabIndex = 5;
+            // 
+            // scoreTB
+            // 
+            this.scoreTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreTB.Location = new System.Drawing.Point(576, 58);
+            this.scoreTB.Name = "scoreTB";
+            this.scoreTB.ReadOnly = true;
+            this.scoreTB.Size = new System.Drawing.Size(147, 30);
+            this.scoreTB.TabIndex = 8;
+            this.scoreTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(35, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 30);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(201, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 30);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // defScoreTB
+            // 
+            this.defScoreTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.defScoreTB.Location = new System.Drawing.Point(90, 58);
+            this.defScoreTB.Name = "defScoreTB";
+            this.defScoreTB.ReadOnly = true;
+            this.defScoreTB.Size = new System.Drawing.Size(105, 30);
+            this.defScoreTB.TabIndex = 5;
+            this.defScoreTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 484);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Slot Machine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox defScoreTB;
+        private System.Windows.Forms.TextBox scoreTB;
+    }
+}
+
---
+++ SlotMachine/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SlotMachine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int score = 1000;
+        int defScore = 100;         
+       
+        static string wDir = Directory.GetCurrentDirectory();
+        static string pDir = Directory.GetParent(wDir).Parent.FullName;
+        string[] fields = Directory.GetFiles(pDir + @"\Resources", "*.png");                  
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            Random random = new Random();
+
+            int n = random.Next(0, 9);
+            int n1 = random.Next(0, 9);
+            int n2 = random.Next(0, 9);
+            
+            Image im = Image.FromFile(fields[n]);
+            Image im1 = Image.FromFile(fields[n1]);
+            Image im2 = Image.FromFile(fields[n2]);
+
+            pictureBox1.Image = im;
+            pictureBox2.Image = im1;
+            pictureBox3.Image = im2;
+
+            if (n == n1 && n == n2)
+            {
+                switch(n)
+                {
+                    case 0:
+                        score += (defScore / 100) * 200;
+                        scoreTB.Text = "$" + score.ToString();
+                        break;
+                    case 1:
+                        score += (defScore / 100) * 300;
+                        scoreTB.Text = "$" + score.ToString();
+                        break;
+                    case 2:
+                        score += (defScore / 100) * 400;
+                        scoreTB.Text = "$" + score.ToString();
+                        break;
+                    case 3:
+                        score += (defScore / 100) * 500;
+                        scoreTB.Text = "$" + score.ToString();
+                        break;
+                    case 4:
+                        score += (defScore / 100) * 1000;
+                        scoreTB.Text = "$" + score.ToString();
+                        break;
+                    case 5:
+                        score += (defScore / 100) * 2000;
+                        scoreTB.Text = "$" + score.ToString();
+                        break;
+                    case 6:
+                        score += (defScore / 100) * 3000;
+                        scoreTB.Text = "$" + score.ToString();
+                        break;
+                    case 7:
+                        score += (defScore / 100) * 5000;
+                        scoreTB.Text = "$" + score.ToString();
+                        break;
+                    case 8:                        
+                        score += (defScore / 100) * 10000;                        
+                        scoreTB.Text = "$" + score.ToString();
+                        break;
+                }
+            }
+            else
+            {
+                score -= defScore;
+                scoreTB.Text = "$" + score.ToString();
+            }
+
+            if (score <= 0)
+            {
+                MessageBox.Show("No more credit", "Game Over");
+                newGame();
+            }
+
+            if (score < defScore)
+            {
+                defScore = score;               
+                defScoreTB.Text = "$" + defScore.ToString();
+            }            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Color hexColor = Color.FromArgb(0xf8c08f);
+            Color rbgColor = Color.FromArgb(hexColor.R, hexColor.G, hexColor.B);
+            panel1.BackColor = rbgColor;
+            Image bImage = new Bitmap(pDir + @"\background.jpg");
+            this.BackgroundImage = bImage;
+            newGame();
+        }
+
+        private void newGame()
+        {
+            score = 1000;
+            defScore = 100;
+            label1.Text = "Credit";
+            label1.BackColor = System.Drawing.Color.Transparent;
+            scoreTB.Text = "$" + score.ToString();
+            defScoreTB.Text = "$" + defScore.ToString();
+            
+            pictureBox1.Image = Image.FromFile(pDir + @"\dollar.png");
+            pictureBox1.BackColor = System.Drawing.Color.Transparent; 
+            
+            pictureBox2.Image = Image.FromFile(pDir + @"\dollar.png");
+            pictureBox2.BackColor = System.Drawing.Color.Transparent;
+
+            pictureBox3.Image = Image.FromFile(pDir + @"\dollar.png");
+            pictureBox3.BackColor = System.Drawing.Color.Transparent;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {                       
+            if (defScore < score)
+            {
+                defScore += 10;
+                defScoreTB.Text = "$" + defScore.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Not enough credit");
+                defScore = score;
+                defScoreTB.Text = "$" + defScore.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (defScore > 10)
+            {
+                defScore -= 10;
+                defScoreTB.Text = "$" + defScore.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Minimum bet is $10");
+                defScore = 10;
+                defScoreTB.Text = "$" + defScore.ToString();
+            }         
+        }                                  
+    }
+}
